b53686b96dbebf10344d9544c80cf33c982ed300
akshay-1a <akshay1.py@gmail.com>
2024-12-01 02:16:07 +0530

    updated metadata

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import Client from './client'
+import { Metadata } from 'next';
+import { generateMetadata as getMetadata } from '@/lib/generate-metadata';
+
+
+export async function generateMetadata(): Promise<Metadata> {
+  return getMetadata({ slug: ['company', 'payment'] })
+}
+
 
 export default function page() {
   return <Client/>
